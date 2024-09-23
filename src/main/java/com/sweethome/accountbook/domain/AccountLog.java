e73d08bea4c107630ebd53af52c235290dbd26a7
nsh <nsh2398@naver.com>
2024-09-23 23:27:29 +0900

    #7 AccountLogDto 생성
    상속받은 AccountLog의 멤버변수도 Builder패턴에서 사용할 수 있도록 superBuilder 애노테이션 추가

--- src/main/java/com/sweethome/accountbook/domain/AccountLog.java
+++ src/main/java/com/sweethome/accountbook/domain/AccountLog.java
@@ -1,14 +1,13 @@
 package com.sweethome.accountbook.domain;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDate;
 
-@Builder
+@SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
