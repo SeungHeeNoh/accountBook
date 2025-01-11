ce55a3686ff83af080660fae493dfcca3702f936
nsh <nsh2398@naver.com>
2025-01-11 23:52:06 +0900

    feat: api, view 둘 다 관리하기 위해서 controller하위 패키지를 api와 view로 분리
    - api의 경우 url 경로가 api부터 시작하도록 변경
    
    #18

--- src/main/java/com/sweethome/accountbook/controller/view/HomeController.java
+++ src/main/java/com/sweethome/accountbook/controller/view/HomeController.java
@@ -1,4 +1,4 @@
-package com.sweethome.accountbook.controller;
+package com.sweethome.accountbook.controller.view;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
