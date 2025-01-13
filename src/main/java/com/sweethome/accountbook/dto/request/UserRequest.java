8bee68b2e4bb1f0943b22962b14355ac227241c1
nsh <nsh2398@naver.com>
2025-01-13 22:09:24 +0900

    feat: 회원가입 test코드 생성 및 UserRequest에 getter/setter/ToString을 추가
    #17

--- src/main/java/com/sweethome/accountbook/dto/request/UserRequest.java
+++ src/main/java/com/sweethome/accountbook/dto/request/UserRequest.java
@@ -1,7 +1,13 @@
 package com.sweethome.accountbook.dto.request;
 
 import com.sweethome.accountbook.domain.User;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@ToString
+@Getter
+@Setter
 public class UserRequest {
 
     private String userId;
@@ -13,4 +19,5 @@ public class UserRequest {
                 .password(password)
                 .build();
     }
+
 }
---
+++ src/test/java/com/sweethome/accountbook/controller/api/UserControllerTest.java
@@ -0,0 +1,82 @@
+package com.sweethome.accountbook.controller.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sweethome.accountbook.dto.request.LogTypeManageRequest;
+import com.sweethome.accountbook.dto.request.UserRequest;
+import com.sweethome.accountbook.dto.response.Response;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserControllerTest {
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private static Stream<Arguments> createUser(){
+        return Stream.of(
+                Arguments.of(
+                        "user insert 성공 케이스",
+                        getUserRequest("yjh"),
+                        createExpectResult(new Response("success", "사용자를 등록하는 데 성공했습니다."))
+                ),
+                Arguments.of(
+                        "중복 userId로 실패하는 케이스",
+                        getUserRequest("nsh"),
+                        createExpectResult(new Response("fail", "동일한 id가 이미 존재하여\n, 등록에 실패했습니다."))
+                )
+        );
+    }
+
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("createUser")
+    @Transactional
+    void givenParam_whenCreateUser_thenReturningResultJson(String displayName, UserRequest requestBody, Map<String, Object> expectResult) throws Exception {
+        // given
+
+        // when & then
+        mockMvc.perform(
+                        post("/api/v1/user")
+                                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                                .content(objectMapper.writeValueAsString(requestBody))
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(expectResult)))
+        ;
+    }
+
+    // fixture
+    private static UserRequest getUserRequest(String userId) {
+        UserRequest requestBody = new UserRequest();
+        requestBody.setUserId(userId);
+        requestBody.setPassword("1234");
+
+        return requestBody;
+    }
+
+    private static Map<String, Object> createExpectResult(Object data) {
+        Map<String, Object> expectResult = new HashMap<>();
+        expectResult.put("data", data);
+
+        return expectResult;
+    }
+}
