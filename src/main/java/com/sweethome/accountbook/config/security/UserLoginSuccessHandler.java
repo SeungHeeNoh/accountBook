266cf9d8617fc3e53b39fbfd681b04341c47ff59
nsh <nsh2398@naver.com>
2025-01-25 16:07:11 +0900

    feat: successHandler에 성공 후 실행할 리다이렉트 로직 구현
    #22

--- src/main/java/com/sweethome/accountbook/config/security/UserLoginSuccessHandler.java
+++ src/main/java/com/sweethome/accountbook/config/security/UserLoginSuccessHandler.java
@@ -8,6 +8,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -19,9 +21,16 @@ public class UserLoginSuccessHandler implements AuthenticationSuccessHandler {
     private final UserService userService;
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         User user = (User) authentication.getPrincipal();
+        SavedRequest savedRequest = new HttpSessionRequestCache().getRequest(request, response);
 
         userService.updateLastLoginAt(user);
+
+        if(savedRequest != null) {
+            response.sendRedirect(savedRequest.getRedirectUrl());
+        } else {
+            response.sendRedirect("/");
+        }
     }
 }
