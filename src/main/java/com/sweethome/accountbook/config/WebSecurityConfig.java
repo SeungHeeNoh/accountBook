658f2719014316f6e6de22d8cddb1b8036afee32
nsh <nsh2398@naver.com>
2025-01-29 00:29:27 +0900

    feat: @EnableWebSecurity annotation 추가
    #22

--- src/main/java/com/sweethome/accountbook/config/WebSecurityConfig.java
+++ src/main/java/com/sweethome/accountbook/config/WebSecurityConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
+@EnableWebSecurity
 @RequiredArgsConstructor
 @Configuration
 public class WebSecurityConfig {
